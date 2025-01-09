0a658f9c9ddc98cbe60d98db068472e474a1e319
Gabie Mfn <gabriellehermesmf@gmail.com>
2025-01-09 17:44:41 +0100

    SvgIcon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg :class="`icon ${customClass}`" aria-hidden="true">
+    <use :xlink:href="`#icon-${name}`" />
+  </svg>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  customClass: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(__dirname, 'src/assets/sprites')],
+      symbolId: 'icon-[name]',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        },
+      },
+    },
+  },
+});
